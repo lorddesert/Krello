f71c82ed9b3701abcfd69678930638fd9bea3cfd
SN <sacha.nocetto@hotmail.com>
2020-03-30 09:14:18 -0300

    update design * change the background color * remove social section temporaly

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,14 +12,14 @@ const Footer = () => {
             Develop by lorddesert,
             iconos diseñados por <a href="https://www.flaticon.es/autores/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.es/" title="Flaticon"> www.flaticon.es</a>
           </div>
-        <div className='Social'>
+        {/* <div className='Social'>
           <div className='Social-Links'>
             <ul>
               <li>Github</li>
               <li>LinkedIn</li>
             </ul>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
